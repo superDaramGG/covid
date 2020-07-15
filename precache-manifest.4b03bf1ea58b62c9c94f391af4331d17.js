ba7b08d0f0d91f0b860c0611b17fe4b418348243
Yeseo <superior817@naver.com>
2020-07-15 15:14:26 +0900

    Updates

---
+++ precache-manifest.4b03bf1ea58b62c9c94f391af4331d17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7f1c6c2ec497d4939d901dc41cd28db",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "8ad6328ad02adf626043",
+    "url": "/covid/static/js/2.76d33ee7.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/covid/static/js/2.76d33ee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af8171d1f549059264d9",
+    "url": "/covid/static/js/main.c991a58d.chunk.js"
+  },
+  {
+    "revision": "b81d16d18ca601f7064b",
+    "url": "/covid/static/js/runtime-main.0fed4ab8.js"
+  }
+]);
